f57ace8690f7b80be04b0627685a12d29fa398a4
alexanderyanthar <alexander.yanthar@outlook.com>
2023-05-30 17:46:30 -0400

    initialized files, created basic html and css styling, initialized firebase SDK

---
+++ app.js
@@ -0,0 +1,31 @@
+ // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+  import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDI-yjPo7uWisxfVDIiDcC-qT_rdekVqrg",
+    authDomain: "real-time-chat-app-19d9f.firebaseapp.com",
+    databaseURL: "https://real-time-chat-app-19d9f-default-rtdb.firebaseio.com",
+    projectId: "real-time-chat-app-19d9f",
+    storageBucket: "real-time-chat-app-19d9f.appspot.com",
+    messagingSenderId: "187015861489",
+    appId: "1:187015861489:web:ce9941e4788bdabcc94550"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const database = getDatabase(app);
+  const dbRef = ref(database);
+
+// Pseudo-code
+// When user hits submit button, a new node is created in the root database and saves that message
+// onValue listens to any new changes, in this case a new message, get's snapshot of data and displays it in the textDisplayContainer
+
+const newMessage {
+    content: ''
+}
+
+
